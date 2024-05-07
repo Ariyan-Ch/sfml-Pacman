b91711f3024738972c3467234f8e0e4b399044eb
Ariyan <ariyan29113@gmail.com>
2024-05-07 22:34:20 +0500

    basic movement and map, uses 2D array and png image

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unistd.h>
 
 //#include <X11/Xlib.h>
 //#undef Drawable
@@ -9,19 +10,143 @@
 //#undef None
 
 #include <SFML/Graphics.hpp>
-
 using namespace sf;
+
+int playerSize = 16;
+
+short int numberMap [36][28] = {
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1},
+        {1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1},
+        {1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,0,1},
+        {1,0,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,0,1},
+        {1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,1},
+        {1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1},
+        {1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1},
+        {1,1,1,1,1,1,0,1,1,0,0,0,0,0,0,0,0,0,0,1,1,0,1,1,1,1,1,1},
+        {1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1},
+        {1,1,1,1,1,1,0,1,1,0,1,0,0,0,0,0,0,1,0,1,1,0,1,1,1,1,1,1},
+        {0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0},
+        {1,1,1,1,1,1,0,1,1,0,1,0,0,0,0,0,0,1,0,1,1,0,1,1,1,1,1,1},
+        {1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1},
+        {1,1,1,1,1,1,0,1,1,0,0,0,0,0,0,0,0,0,0,1,1,0,1,1,1,1,1,1},
+        {1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1},
+        {1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1},
+        {1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1},
+        {1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,1},
+        {1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1},
+        {1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1},
+        {1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,1},
+        {1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1},
+        {1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+};
 
 //"-lX11"
 
 int main(){
 
 
-    RenderWindow window(VideoMode(1040,800), "Pac Man");
 
-    while(window.isOpen()){
+ sf::RenderWindow window(sf::VideoMode(448, 576), "Pac Man");
 
+    // Load the image
+    sf::Texture texture;
+    if (!texture.loadFromFile("resources/map.png"))
+    {
+        // Error loading image
+        std::cout<<"Could not load map image"<<std::endl;
+        return 1;
+    }
+
+    // Create the player (a simple rectangle for now)
+    sf::RectangleShape player(sf::Vector2f(playerSize, playerSize));
+    player.setFillColor(sf::Color::Green);
+    player.setPosition(16*1, 16*4);
+
+    sf::Vector2i playerPosition(1, 4);
+
+
+    // Game loop
+    while (window.isOpen())
+    {
+        // Handle events
+        usleep(3000);
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+
+            if (event.type == Event::KeyPressed) {
+                switch (event.key.code) {
+                    case Keyboard::W:
+                        if (numberMap[playerPosition.y-1][playerPosition.x] != 1){
+                            playerPosition.y -=1;
+                            player.setPosition(playerPosition.x*playerSize,playerPosition.y*playerSize);
+                        }
+                        break;
+                    case Keyboard::A:
+                        if (numberMap[playerPosition.y][playerPosition.x-1] != 1){
+                            playerPosition.x -=1;
+                            player.setPosition(playerPosition.x*playerSize, playerPosition.y*playerSize);
+                        }
+                        break;
+                    case Keyboard::S:
+                        if (numberMap[playerPosition.y+1][playerPosition.x] != 1){
+                            playerPosition.y +=1;
+                            player.setPosition(playerPosition.x*playerSize,playerPosition.y*playerSize);
+                        }
+                        break;
+                    case Keyboard::D:
+                        if (numberMap[playerPosition.y][playerPosition.x+1] != 1){
+                            playerPosition.x += 1;
+                            player.setPosition(playerPosition.x*playerSize,playerPosition.y*playerSize);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        // Check boundary conditions
+        sf::FloatRect playerBounds = player.getGlobalBounds();
+        if (playerBounds.left < 0)
+            player.setPosition(0, player.getPosition().y);
+        if (playerBounds.top < 0)
+            player.setPosition(player.getPosition().x, 0);
+        if (playerBounds.left + playerBounds.width > window.getSize().x)
+            player.setPosition(window.getSize().x - playerBounds.width, player.getPosition().y);
+        if (playerBounds.top + playerBounds.height > window.getSize().y)
+            player.setPosition(player.getPosition().x, window.getSize().y - playerBounds.height);
+
+        // Clear the window
+        window.clear();
+
+        // Draw the map
+        sf::Sprite map(texture);
+ 
+        window.draw(map);
+
+        // Draw the player
+        window.draw(player);
+
+        // Display everything
         window.display();
     }
-    return 0;
+
 }
