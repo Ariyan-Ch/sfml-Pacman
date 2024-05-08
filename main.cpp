d650222b54d077224093c74ec2d6c8678ac058e8
Ariyan <ariyan29113@gmail.com>
2024-05-09 00:48:18 +0500

    create ghost, basic player following ai, pellet img change

--- main.cpp
+++ main.cpp
@@ -10,10 +10,12 @@
 //"-lX11"
 
 #include <unistd.h>
+#include <math.h>
 #include <SFML/Graphics.hpp>
 using namespace std;
 using namespace sf;
 
+//==== DECLARATION OF ALL GLOBAL VARIABLES
 const float movementSpeed = 0.1f;
 const int playerSize = 16;
 
@@ -55,6 +57,111 @@ short int numberMap [36][28] = {
         {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
         {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
 };
+
+
+//================================================================------...... DECLARATION OF GLOBAL VARS
+//========================== Ghost Stuff
+struct Ghost{
+    Vector2i position;
+    Vector2i velocity;
+    const float movementSpeed = 0.15f;
+    float elapsed;
+    sf::RectangleShape* character;
+
+    Ghost(int x=0, int y=0, int vx = 0, int vy = 0){
+        position.x = 1;
+        position.y = 10;
+        velocity.x = 0;
+        velocity.y = -1;
+        elapsed = 0.0f;
+
+        character = new sf::RectangleShape(sf::Vector2f(playerSize, playerSize));
+        character->setFillColor(sf::Color::Red);
+        character->setPosition(16*position.x, 16*position.y);
+    }
+};
+
+//takes ghost coords and player coords, returns distance between them.
+double calculateDistance(int x1, int y1, int x2, int y2) {
+    double deltaX = x1 - x2;
+    double deltaY = y1 - y2;
+    deltaX = deltaX * deltaX;
+    deltaY = deltaY * deltaY;
+
+    return std::pow(deltaX+deltaY, 0.5);;
+}
+
+//takes ghost structure and player position as parameters. returns nothing.
+void ghostMovement(struct Ghost& g, Vector2i& playerPosition){
+
+ 
+
+     if (g.elapsed >= g.movementSpeed) { // Check if enough time has passed
+        double minDistance = 5000;
+        double dist;
+        if(numberMap[g.position.y][g.position.x-1] == 0 ){
+            dist = calculateDistance(g.position.x-1, g.position.y, playerPosition.x, playerPosition.y);
+            if(minDistance > dist){
+                minDistance = dist;
+                g.velocity.x = -1;
+                g.velocity.y = 0;
+            }
+            //std::cout<<minDistance<<std::endl;
+        }
+        if(numberMap[g.position.y-1][g.position.x] == 0 ){
+            dist = calculateDistance(g.position.x, g.position.y-1, playerPosition.x, playerPosition.y);
+            if(minDistance > dist){
+                minDistance = dist;
+                g.velocity.x = 0;
+                g.velocity.y = -1;
+            }
+        }
+        if(numberMap[g.position.y][g.position.x+1] == 0 ){
+            dist = calculateDistance(g.position.x+1, g.position.y, playerPosition.x, playerPosition.y);
+            if(minDistance > dist){
+                minDistance = dist;
+                g.velocity.x = 1;
+                g.velocity.y = 0;
+            }
+        }
+        if(numberMap[g.position.y+1][g.position.x] == 0 ){
+            dist = calculateDistance(g.position.x, g.position.y+1, playerPosition.x, playerPosition.y);
+            if(minDistance > dist){
+                minDistance = dist;
+                g.velocity.x = 0;
+                g.velocity.y = 1;
+            }
+        }
+            Vector2i newPosition = g.position + g.velocity;
+            
+            //std::cout<<g.elapsed<<std::endl;
+            if (numberMap[newPosition.y][newPosition.x] != 1) {
+                g.position = newPosition;
+                g.character->setPosition(newPosition.x * playerSize, newPosition.y * playerSize);
+            }
+
+
+            //teleportation
+            /*
+            if (newPosition.y == 17 && (newPosition.x < 0 || newPosition.x > 27))
+            {
+                if (newPosition.x < 0)
+                  {
+                    newPosition.x += 28;
+                    player.setPosition( newPosition.x * playerSize, playerPosition.y * playerSize);
+                  }
+                else if (newPosition.x > 27)
+                       {
+                        newPosition.x -= 28;
+                        player.setPosition(newPosition.x * playerSize, playerPosition.y * playerSize);
+                       }
+                       playerPosition = newPosition;
+            }*/
+            g.elapsed = 0.0f; // Reset the elapsed time
+        }
+}
+
+//========================== Ghost Stuff
 
 
 void constantMovement(Window& window, Vector2i& velocity, Vector2i& playerPosition, RectangleShape& player, float& elapsed)
@@ -124,6 +231,8 @@ void pelletCollision(Sprite*& pellets, int size, bool*& ifcollected, int& score,
 
 }
 
+
+
 int main(){
 
  sf::RenderWindow window(sf::VideoMode(448, 576), "Pac Man");
@@ -139,7 +248,7 @@ int main(){
     }
 
     Texture pellet_texture;
-    if(!pellet_texture.loadFromFile("resources/pellet.png")){
+    if(!pellet_texture.loadFromFile("resources/smallPellet.png")){
         // Error loading image
         std::cout<<"Could not load pallet image"<<std::endl;
         return 1;
@@ -158,6 +267,7 @@ int main(){
     sf::RectangleShape player(sf::Vector2f(playerSize, playerSize));
     player.setFillColor(sf::Color::Yellow);
     player.setPosition(16*1, 16*4);
+
     sf::Vector2i playerPosition(1, 4);
     sf::Vector2i velocity (0, 0);
 
@@ -187,7 +297,6 @@ int main(){
               {
                 pellets[k].setTexture(pellet_texture);
                 pellets[k].setPosition(j * 16, i * 16);
-                pellets[k].setScale(0.2,0.2);
                 k++;
               }
           }
@@ -213,17 +322,21 @@ int main(){
     r_score.setCharacterSize(20);
     r_lives.setCharacterSize(20);
     //================================ lives + scoreboard setup
-  
-        
+    Ghost g1;
+    float seconds = 0.0f;
     // Game loop
     while (window.isOpen()) {
         //Update elasped
-        elapsed += clock.restart().asSeconds(); 
+        seconds = clock.restart().asSeconds();
+        elapsed += seconds;
+        g1.elapsed += seconds;
+        
         //Handle events
         usleep(3000);
         
 
        constantMovement(window, velocity, playerPosition, player, elapsed);
+       ghostMovement(g1,playerPosition);
        pelletCollision(pellets, counter, ifcollected, score, player);
        
 
@@ -240,14 +353,15 @@ int main(){
 
         window.draw(map);
         window.draw(player);
- 
+        window.draw(*g1.character);
+
         window.draw(t_score);
         window.draw(t_lives);
 
         r_score.setString(to_string(score));
-        std::cout<<to_string(score)<<std::endl;
+        //std::cout<<to_string(score)<<std::endl;
 
-        std::cout<<lives<<std::endl;
+        //std::cout<<lives<<std::endl;
         window.draw(r_score);
         window.draw(r_lives);
         window.display();
